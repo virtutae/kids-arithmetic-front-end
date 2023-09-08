95ac0f56b036576c6627c289183888f616e9e56e
virtutae <virtutae@yahoo.it>
2023-09-08 13:14:03 +0100

    added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+function Footer(): JSX.Element {
+    return (
+        <>
+            <div className="footer">
+                <a
+                    href="https://github.com/virtutae/kids-arithmetic-front-end"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Front end repo
+                </a>
+                <br />
+                <a
+                    href="https://github.com/virtutae/kids-arithmetic-back-end"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Back end repo
+                </a>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
